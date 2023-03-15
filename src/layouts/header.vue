5df51f38ac34a434524cde47be11ef1fbad7142e
Faiz <agustianto.d19@gmail.com>
2023-03-15 07:32:03 +0700

    fix header.vue

--- src/layouts/header.vue
+++ src/layouts/header.vue
@@ -2,18 +2,82 @@
   <q-layout view="hHh lpR fF2">
     <q-header bordered="false" class="header-bg row items-center justify-evenly">
       <q-icon name="img:icons/logo.png" class="logo" />
-      <p class="header-text jakarta-md q-mb-none">Kategori</p>
+      <q-btn flat color="black" label="Kategori" class="header-text jakarta-md" no-caps to="kategori-fruits" />
       <q-input rounded outlined class="search-bar" v-model="search" placeholder="Search" dense>
         <template v-slot:prepend>
           <q-icon name="search" />
         </template>
       </q-input>
-      <q-btn flat round color="hijau60" icon="fa-solid fa-cart-shoppin" />
-      <q-btn v-if="loggedIn" flat round color="hijau60" icon="fa-solid fa-cart-shoppin" />
+      <q-btn flat round color="black" icon="fa-solid fa-cart-shopping" />
+      <q-btn-dropdown v-if="loggedIn" icon="img:icons/notification.png" no-caret flat v-model="showDropdown"
+        @mouseenter="showDropdown = true" dense>
+        <div @mouseleave="showDropdown = false">
+          <div class="row justify-evenly items-end">
+            <q-item-label class="cursor-pointer" :class="{ 'active-page': currentPage }"
+              @click="currentPage = true">Transaksi</q-item-label>
+            <q-item-label class="cursor-pointer" :class="{ 'active-page': !currentPage }"
+              @click="currentPage = false">Informasi</q-item-label>
+          </div>
+          <q-item v-if="currentPage">
+            <q-item-section>
+              <q-item clickable v-close-popup @click="onItemClick">
+                <q-item-section avatar>
+                  <q-avatar>
+                    <q-icon name="fa-solid fa-arrows-rotate" />
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="jakarta-md payment-label">Pesanan Diproses</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup @click="onItemClick">
+                <q-item-section avatar>
+                  <q-avatar>
+                    <q-icon name="fa-solid fa-truck-fast" />
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="jakarta-md payment-label">Sedang Dikirm</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup @click="onItemClick">
+                <q-item-section avatar>
+                  <q-avatar>
+                    <q-icon name="fa-regular fa-file-lines" />
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="jakarta-md payment-label">Riwayat Transaksi</q-item-label>
+                </q-item-section>
+              </q-item>
+
+
+              <q-item clickable v-close-popup @click="onItemClick">
+                <q-item-section avatar>
+                  <q-avatar>
+                    <q-icon name="fa-solid fa-arrows-rotate" />
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="jakarta-md payment-label">Pesanan diproses</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-item-section>
+          </q-item>
+          <q-item v-else="currentPage" v-for="notification in notifications" :key="notification.id" class="q-mt-sm">
+            <q-item-section>
+              <q-item-label>{{ notification.title }}</q-item-label>
+              <q-item-label caption style="max-width: 270px;">{{ notification.message }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+      </q-btn-dropdown>
       <div class="seperator" />
-      <div v-if="loggedIn" class="row">
-        <q-img :src="user.avatar" size="29px" inline />
-        <P class="name jakarta-sb q-mb-none" inline>{{ user.first_name }}</P>
+      <div v-if="loggedIn" class="row items-center">
+        <q-icon name="img:icons\pofile.png" size="29px" />
+        <P class="name jakarta-sb q-mb-none q-ml-sm" inline>{{ user.first_name }}</P>
       </div>
       <div v-else class="roww">
         <q-btn outline color="hijau60" label="Masuk" no-caps class="jakarta-md q-mx-md" to="/" />
@@ -36,7 +100,14 @@ export default {
     return {
       search: ref(null),
       loggedIn: false,
-      user: null
+      user: null,
+      currentPage: ref(true),
+      showDropdown: ref(false),
+      notificationIcon: 'icons/bell-regular.svg',
+      notifications: [
+        { id: 1, title: 'Pembayaranmu sudah terverifikasi', message: 'Pembayaran kamu sudah kami terima, mohon tunggu konfirmasi selanjutnya.' },
+        { id: 2, title: 'Sobat Buah, suaramu sangat berarti !', message: 'Agar Buah.in bisa melayani lebih baik, jawab pertanyaan singkat ini yuk !' },
+      ]
     }
   },
   async mounted() {
@@ -92,5 +163,14 @@ export default {
 .name {
   font-size: 14px;
   color: #323741;
+}
+
+.active-page {
+  border-bottom: 2px solid #116530;
+  color: #116530;
+}
+
+.toggle-text {
+  color: #ADAFB3;
 }
 </style>
